b146f0d1b4696e19e29fab74d77f951e61554e8e
2012048143 <2012048143@ust-ics.mygbiz.com>
2014-11-19 20:31:14 +0800

    first commit

---
+++ app/src/main/java/com/example/teddy/es_tify/EnterPage.java
@@ -0,0 +1,92 @@
+package com.example.teddy.es_tify;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.TextView;
+
+import com.example.teddy.es_tify.R;
+
+public class EnterPage extends ActionBarActivity {
+
+    public void refresh(View view) {
+        Intent intent = new Intent(this, EnterPage.class);
+        startActivity(intent);
+
+    }
+
+    public void information(View view) {
+        Intent intent = new Intent(this, InformationPage.class);
+        startActivity(intent);
+
+    }
+    public void information2(View view) {
+        Intent intent = new Intent(this, information_page2.class);
+        startActivity(intent);
+
+    }
+    public void information3(View view) {
+        Intent intent = new Intent(this, information_page3.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_page);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+    }
+
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.enter_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_enter_page, container, false);
+            return rootView;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/teddy/es_tify/HomePage.java
@@ -0,0 +1,58 @@
+package com.example.teddy.es_tify;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class HomePage extends ActionBarActivity {
+
+    public void start(View view) {
+        Intent intent = new Intent(this, StartPage.class);
+        startActivity(intent);
+
+    }
+
+    public void about(View view) {
+        Intent intent = new Intent(this, AboutPage.class);
+        startActivity(intent);
+
+    }
+
+    public void credits(View view) {
+        Intent intent = new Intent(this, CreditPage.class);
+        startActivity(intent);
+
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home_page, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
